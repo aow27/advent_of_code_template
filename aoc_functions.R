92154e480643d96561be72f0d7b16e0a221f7138
Alice Whiteway <alicewhiteway@gmail.com>
2025-11-30 12:14:57 +0000

    Changed decription of how cookie is saved

--- aoc_functions.R
+++ aoc_functions.R
@@ -3,7 +3,7 @@
 
 
 # Requires knowing the AoC session cookie, which is typically found in the storage section of a web inspector
-# I've then saved this in my Rprofile file which isn't publicly shared
+# I've then saved this into a cookie.txt which isn't publicly shared
 
 download_advent <- function(year,
                             day,
